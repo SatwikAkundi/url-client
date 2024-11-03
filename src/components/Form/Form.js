ff18b7e747e05d9a997f75960f2d29df9a3fd0cc
userName <user@gmail.com>
2024-11-03 23:21:33 +0530

    first-update

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import styles from "./Form.module.css";
 import axios from "axios";
 
-const serverBase = "http://localhost:5001";
-console.log(serverBase.url);
+// Use environment variable for server base URL
+const serverBase = process.env.REACT_APP_SERVERURL || "http://localhost:5001";
+
 function is_url(str) {
   let exp = new RegExp(
     /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
@@ -12,7 +13,7 @@ function is_url(str) {
 }
 
 function fix_url(url) {
-  if (url.substring(0, 7) === "http://" || url.substring(0, 8) === "https://") {
+  if (url.startsWith("http://") || url.startsWith("https://")) {
     return url;
   } else {
     return `http://${url}`;
@@ -43,11 +44,11 @@ export default function Form(props) {
     axios
       .post(`${serverBase}/short`, postData)
       .then((res) => {
-        props.setFetchedData(res.data[0])
-        console.log(res)
+        props.setFetchedData(res.data[0]);
+        console.log(res);
       })
       .catch((err) => {
-        props.setErrMsg("Something Went Wrong.");
+        props.setErrMsg("Something went wrong.");
         props.setLoading(false);
         console.error(err);
       });
